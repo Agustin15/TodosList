87991375e99e5d7da95cfae80d7b1e2a6939d08a
Agustin15 <agus20m05@gmail.com>
2025-08-21 20:05:06 -0300

    TaskProvider deleted of FilePage

--- frontEnd/src/components/pages/FilesPage.jsx
+++ frontEnd/src/components/pages/FilesPage.jsx
@@ -1,13 +1,10 @@
 import { Files } from "../files/Files";
 import { FilesProvider } from "../../context/FilesContext";
-import { TaskProvider } from "../../context/TaskContext";
 
 export const FilesPage = () => {
   return (
-    <TaskProvider>
-      <FilesProvider>
-        <Files />
-      </FilesProvider>
-    </TaskProvider>
+    <FilesProvider>
+      <Files />
+    </FilesProvider>
   );
 };
